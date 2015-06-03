316fde01e8ad7c2a58aa67c96dba27f0394ed2bc
Jose A. Ruiz <ja_rd@outlook.com>
2015-06-03 21:09:51 +0100

    Test case "blinky" adapted to latest makefile changes. Not really a test and not suitable for regressions but may come in handy to bring up a new board.

--- sw/blinky/blinky.c
+++ sw/blinky/blinky.c
@@ -2,6 +2,8 @@
     @file blinky.c
     @brief LED blinker.
     
+    This program is meant to sanity-check a board. Since it never terminates,
+    it is not really suitable for use in automated tests or regression tests. 
 */
 
 #include <stdio.h>
@@ -16,17 +18,17 @@ int main()
     volatile int i, count;
     
     /* display a message on the console, if there's any... */
-    printf("\n\nBlinky: free running counter on GPIO 0 port pins.\n\n");
+    printf("\nBlinky: free running counter on GPIO 0 port pins.\n\n");
     printf("compile time: " __DATE__ " -- " __TIME__ "\n");
-    printf("gcc version:  " __VERSION__ "\n");
+    printf("gcc version:  " __VERSION__ "\n\n");
     
     /* ...and run a count on the pins of GPIO port 0. */
+    printf("ENTERING ENDLESS LOOP. This program is not suitable for automated tests!\n\n");
     count = 0;
     while(1){
         hal_gpio_write(0, count++);
         for(i=0;i<1000;i++);
     }
-    
     
     return 0;
 }
