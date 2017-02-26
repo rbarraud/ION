cba80158126549de8a6bd53ae24e540f18275886
jaruiz <ja_rd@outlook.com>
2017-02-26 22:51:56 +0000

    Removed old vars from makefile fragment, left behind from local copy of project.

--- sw/Toolchain.mk
+++ sw/Toolchain.mk
@@ -1,6 +1,4 @@
-
+# Toolchain config. Point this at your own MIPS toolchain.
+# (Any will do, we're not using the libraries yet.)
 MAKEHEX = ../../tools/makehex/makehex.py
-
-GCC_WARNS  = -Werror -Wall -Wextra -Wshadow -Wundef -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings
-GCC_WARNS += -Wredundant-decls -Wstrict-prototypes -Wmissing-prototypes -pedantic # -Wconversion
 TOOLCHAIN_PREFIX = /home/jaruiz/dev/tools/buildRoot/buildroot-2016.11.2/output/host/usr/bin/mipsel-buildroot-linux-uclibc-
