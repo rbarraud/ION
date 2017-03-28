ae0253e6f0d0634f1f890229ae7332d024b32c83
jaruiz <ja_rd@outlook.com>
2017-03-28 20:38:00 +0100

    Modified makefile for 'cputest' to produce Verilog ROM init include file.

--- sw/Toolchain.mk
+++ sw/Toolchain.mk
@@ -1,4 +1,7 @@
 # Toolchain config. Point this at your own MIPS toolchain.
 # (Any will do, we're not using the libraries yet.)
-MAKEHEX = ../../tools/makehex/makehex.py
 TOOLCHAIN_PREFIX = /home/jaruiz/dev/tools/buildRoot/buildroot-2016.11.2/output/host/usr/bin/mipsel-buildroot-linux-uclibc-
+
+# Utility scripts included with this project.
+MAKEHEX = ../../tools/makehex/makehex.py
+HEX2ROM = ../../tools/makehex/hex2rom.py
